34170032955e50204b3085b44693cbb4e8952bea
Jacob Silvs <js6135@nyu.edu>
2016-01-14 10:58:35 -0500

    Update BH_converter.py

--- BH_converter.py
+++ BH_converter.py
@@ -1,3 +1,26 @@
+import math
+import sys
+
+class case_selector(Exception):
+   def __init__(self, value): # overridden to ensure we've got a value argument
+      Exception.__init__(self, value)
+
+def switch(variable):
+   raise case_selector(variable)
+
+def case(value):
+   exclass, exobj, tb = sys.exc_info()
+   if exclass is case_selector and exobj.args[0] == value: return exclass
+   return None
+
+def multicase(*values):
+   exclass, exobj, tb = sys.exc_info()
+   if exclass is case_selector and exobj.args[0] in values: return exclass
+   return None
+
+
+
+
 def binary(y):
 	rem = 0
 	array = []
@@ -32,8 +55,73 @@ def hexer(x):
 			string+= str(array[i])
 	return string
 
-answer = raw_input("yes enter a number to be converted: ")
 
-print "binary: " +binary(int(answer))
 
-print "hexidecimal: " +hexer(int(answer))
+
+
+def bin_to_dec(binary):
+
+	rev = binary[::-1]
+	total=0
+	for i in range(len(rev)):
+		total+= math.pow(2,i)*int(rev[i])
+	return total
+	
+
+def hex_to_dec(hex):
+	rev = hex[::-1]
+	total=0
+	for i in range(len(rev)):
+		try:
+			switch(rev[i])
+		except multicase(0,1,2,3,4,5,6,7,8,9):
+			total+= math.pow(16,i)*int(rev[i])
+		except case('a'):
+			total+= math.pow(16,i)*10
+		except case('b'):
+			total+= math.pow(16,i)*11
+		except case('c'):
+			total+= math.pow(16,i)*12
+		except case('d'):
+			total+= math.pow(16,i)*13
+		except case('e'):
+			total+= math.pow(16,i)*14
+		except case('f'):
+			total+= math.pow(16,i)*15
+	return total
+
+
+
+def hex_to_dec_other(hex):
+	rev = hex[::-1]
+	total=0
+	for i in range(len(rev)):
+		if rev[i]=='a':
+			total+= math.pow(16,i)*10
+		elif rev[i]=='b':
+			total+= math.pow(16,i)*11
+		elif rev[i]=='c':
+			total+= math.pow(16,i)*12
+		elif rev[i]=='d':
+			total+= math.pow(16,i)*13
+		elif rev[i]=='e':
+			total+= math.pow(16,i)*14
+		elif rev[i]=='f':
+			total+= math.pow(16,i)*15
+		else:
+			total+= math.pow(16,i)*int(rev[i])
+	return total
+
+
+
+
+
+
+if __name__=='__main__':
+	answer = raw_input("yes enter a number to be converted: ")
+
+	print "binary: " +binary(int(answer))
+	print "hexidecimal: " +hexer(int(answer))
+
+	print bin_to_dec("111")
+	print hex_to_dec("af")
