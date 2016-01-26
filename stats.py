332bcaf065be1eb37f0323bc21e8e10a01f68f48
Jacob Silvs <js6135@nyu.edu>
2016-01-26 13:34:24 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,103 +1,76 @@
 import math
 
-'''
-
-def findSlope(x , string):
-	slope = ""
-	foundX = ""
-	Yintercept = ""
-	for i in range(len(string)):
-		if (string[i] !="x" and foundX != "x"):
-			slope+=string[i]
-		else: 
-			foundX = "x"
-
-		if (foundX == "x" and string[i]!="x" and string[i] != "+"):
-			Yintercept += string[i]
-
-	print int(slope)*x+int(Yintercept)
-
-'''
-
-
-
-
-
-'''
-Takes in an array of data
-then it prints the important info
-no return
-'''
-def stats(data):
-	
-	print "average " + str(sum(data)/len(data))
-
-
-	length = len(data)
-	df = length - 1
-
-
-	sumsqrt = 0
-	sqrtsum = 0
-
-	for integer in range(0,len(data)-1):
-		sumsqrt += data[integer] ** 2
-		sqrtsum += data[integer]
-
-	sqrtsum = sqrtsum**2
-
-	result = (sumsqrt + (sqrtsum/length))/df
-
-
-	print "variance",
-	print result
-
-	print "Standard Deviation",
-	print math.sqrt(result)
-
-
-
-
-array= [1, 4, 2, 5]
+array= [8.0,11.0,0.0,4.0,7.0,8.0,10.0,5.0,8.0,3.0]
 
 array.sort()
 
 
+
 def mode(array):
+
 	return array[len(array)-1]
 
 def median(array):
+
 	if len(array)%2==0:
 		return (array[(len(array)-1)/2]+array[(len(array))/2])/2
 	else:
 		return array[(len(array)-1)/2]
 
 def mean(array):
+
 	sum=0
-	for i in range(0, len(array)-1):
+	for i in range(0, len(array)):
 		sum += array[i]
 
-	return float(sum)/len(array)
+	return float(sum)/float(len(array))
+
+
+def variance(data):
+
+	length = len(data)
+	df = length - 1
+
+	sumsqrt = 0
+	sqrtsum = 0
+
+	for integer in range(0,len(data)):
+		sumsqrt += data[integer] *data[integer]
+		sqrtsum += data[integer]
+
+	sqrtsum *= sqrtsum
+
+	result = (sumsqrt - (sqrtsum/length))/df
+
+	return result
+
+
+def sd(variance):
+
+	return math.sqrt(variance)
+
+
+
+def t_value(array, sd, pop_ave):
+
+	result = (mean(array)-pop_ave)/(sd/math.sqrt(len(array)))
+ 	return result
 
 
 
 
-print mean(array)
-print median(array)
-print mode(array)
+# run the methods for all the statistics
+
+print "Mean: "+str(mean(array))
+print "Median: "+str(median(array))
+print "Mode: "+str(mode(array))
 
 
-
-'''
-string = raw_input("please enter your function: ")
-findSlope(7, string)
-x = 7
-'''
-
-stats([5.0, 7.0, 3.0, 2.0])
+print "Standard Deviation: "+str(sd(variance(array)))
+print "Variance: "+str(variance(array))
 
 
+pop = raw_input("please enter the population average: ")
+print "t value: ",
+print str(t_value(array, sd(variance(array)), float(pop)))
 
-
-
- 
